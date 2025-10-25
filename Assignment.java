065a02d9b34c0cc76d92edfebd9b054a61992ef6
Alec Kingsley <aleckingsley@gmail.com>
2025-10-25 16:46:08 -0400

    Create Assignment.java

---
+++ Assignment.java
@@ -0,0 +1,57 @@
+/**
+ * Assignment class for SHC language.
+ *
+ * @author Alec Kingsley
+ */
+public final class Variable extends Parsable {
+	/** thing to assign to */
+  private Variable assignee = null;
+  /** value to assign with */
+  private OrExpression value;
+  
+	/**
+	 * Constructor for the {@code Assignment} class.
+	 *
+	 * @param assignee - the thing whose value is being changed
+	 * @param value - the value of the expression
+	 * @param lineIdx - index of the line {@code Parsable} starts at
+	 * @param charIdx - char index in the line {@code Parsable} starts at
+	 */
+	public Assignment(Variable assignee, OrExpression value, int lineIdx, int charIdx) {
+		super(lineIdx, charIdx);
+		this.assignee = assignee;
+		this.value = value;
+	}
+
+	/**
+	 * Constructor for the {@code Assignment} class.
+	 *
+	 * @param value - the value of the expression
+	 * @param lineIdx - index of the line {@code Parsable} starts at
+	 * @param charIdx - char index in the line {@code Parsable} starts at
+	 */
+	public Assignment(OrExpression value, int lineIdx, int charIdx) {
+		super(lineIdx, charIdx);
+		this.value = value;
+	}
+
+	/** getter method for {@code assignee} */
+	public Variable getAssignee() {
+		if (assignee == null) {
+			throw new RuntimeException("assignment assignee not defined");
+		}
+		return assignee;
+	}
+
+	/**
+	 * Return true iff {@code this} has assignee.
+	 */
+	public boolean hasAssignee() {
+		return assignee != null;
+	}
+
+	/** getter method for {@code value} */
+  public OrExpression getValue() {
+  	return value;
+  }
+}
