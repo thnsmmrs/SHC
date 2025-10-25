90a0eaad9d79bf4e3ef98b6a1b324cb03cddd8d3
Alec Kingsley <aleckingsley@gmail.com>
2025-10-25 18:47:40 -0400

    Fix Expression.java

--- Expression.java
+++ Expression.java
@@ -5,22 +5,22 @@
  */
 public final class Expression extends Parsable {
 	/** list of expressions */
-	private AdditiveExpression[] expressions;
+	private Assignment[] expressions;
   
 	/**
-	 * Constructor for the {@code AdditiveExpression} class.
+	 * Constructor for the {@code Expression} class.
 	 *
 	 * @param expressions - the list of expressions
 	 * @param lineIdx - index of the line {@code Parsable} starts at
 	 * @param charIdx - char index in the line {@code Parsable} starts at
 	 */
-	public Expression(AdditiveExpression[] expressions, int lineIdx, int charIdx) {
+	public Expression(Assignment[] expressions, int lineIdx, int charIdx) {
 		super(lineIdx, charIdx);
 		this.expressions = expressions;
 	}
 
 	/** getter method for {@code expressions} */
-  public AdditiveExpression[] getExpressions() {
+  public Assignment[] getExpressions() {
   	return expressions;
   }
 }
