e25c76ff1e218758308b35bf5d81df75c9beff0f
Alec Kingsley <aleckingsley@gmail.com>
2025-10-25 15:20:08 -0400

    Add examples and pointers to Variable

--- Variable.java
+++ Variable.java
@@ -7,21 +7,24 @@ public final class Variable extends Parsable {
   /** the name of the variable */
   private String name;
   /** type of the variable */
-	private BHC type;
+	private SHC type;
+	/** number of references */
+	private int nReferences;
 
 	/**
 	 * Constructor for the {@code Variable} class.
 	 *
 	 * @param name - the name of the variable
 	 * @param type - the type of the variable
+	 * @param nReferences - the number of references away from the data
 	 * @param lineIdx - index of the line {@code Parsable} starts at
 	 * @param charIdx - char index in the line {@code Parsable} starts at
 	 */
-	public Variable(String name, String type, int lineIdx, int charIdx) {
+	public Variable(String name, SHC type, int nReferences, int lineIdx, int charIdx) {
 		super(lineIdx, charIdx);
 		this.name = name;
 		this.type = type;
-		this.scope = scope;
+		this.nReferences = nReferences;
 	}
 
 	/** getter method for {@code name} */
@@ -32,8 +35,15 @@ public final class Variable extends Parsable {
 	/**
 	 * Getter method for {@code type}
 	 */
-	public MantleClass getType() {
+	public SHC getType() {
 		return type;
+	}
+
+	/**
+	 * Getter method for {@code type}
+	 */
+	public SHC getNReferences() {
+		return nReferences;
 	}
 
 	/**
