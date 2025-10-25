7a9b4d24c52cc9bd34b7bb23678f6d91a4628ba8
Alec Kingsley <aleckingsley@gmail.com>
2025-10-25 13:41:02 -0400

    Create BNF

---
+++ SHC.java
@@ -0,0 +1,43 @@
+// This enum represents our tokens
+enum Mantle {
+	// Keywords
+	AND,
+	BEGIN,
+	ELSE,
+	END,
+	IF,
+	IN,
+	NOT,
+	OR,
+	FUN,
+	PUBLIC,
+	THEN,
+	CHAR,
+	VOID,
+	INT,
+	// Symbols
+	ADD, 
+	SUBTRACT,
+	MULTIPLY,
+	DIVIDE,
+	ASSIGN,
+	EQUAL,
+	LESS,
+	COLON,
+	SEMICOLON,
+	PERIOD,
+	COMMA,
+	LPAREN,
+	RPAREN,
+	LSQUARE,
+	RSQUARE,
+	LCURL,
+	RCURL,
+	LSHIFT, // <<
+	RSHIFT, // >> 
+	// Special
+	ID,
+	STRING_LITERAL,
+	EOS,
+	ERROR
+}
