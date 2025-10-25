a0dffe5b15c104ba79a7a90f33c855934d840dff
Alec Kingsley <aleckingsley@gmail.com>
2025-10-25 15:46:32 -0400

    Create Factor.java

---
+++ Factor.java
@@ -0,0 +1,61 @@
+/**
+ * Factor class for SHC language.
+ *
+ * @author Alec Kingsley
+ */
+public sealed interface Factor permits Factor.Var, Factor.Const, Factor.Str, Factor.Call {
+	final class Var extends Parsable implements Factor {
+		/** variable */
+		private Variable variable;
+		/** number of references away from value */
+		private int nReferences;
+
+		/**
+		 * Constructor for a variable factor.
+		 *
+		 * @param name - name of the variable
+		 * @param lineIdx - index of the line {@code Parsable} starts at
+		 * @param charIdx - char index in the line {@code Parsable} starts at
+		 */
+		public Var(Variable variable, int nReferences, int lineIdx, int charIdx) {
+			super(lineIdx, charIdx);
+			this.variable = variable;
+			this.nReferences = nReferences;
+		}
+
+		@Override
+		public String toString() {
+			name = "";
+			for (int i = 0; i < nReferences; i++) {
+				name += "^";
+			}
+			name += variable.toString();
+		}
+	}
+
+	public record Const(int constant, int lineIdx, int charIdx) implements Factor {
+		@Override
+		public String toString() {
+			return "" + constant;
+		}
+	}
+
+	public record Str(String string, int lineIdx, int charIdx) implements Factor {
+		@Override
+		public String toString() {
+			return "\"" + string + "\"";
+		}
+	}
+
+	public record Call(Function fun, Expression[] arguments, int lineIdx, int charIdx) implements Factor {
+		@Override
+		public String toString() {
+			value = fun.getName() + "(";
+			for (argument : arguments) {
+				value += argument.toString();
+			}
+			value += ")";
+			return value;
+		}
+	}
+}
